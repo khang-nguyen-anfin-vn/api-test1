ee909e47dc6c685f9ee27784e6792e4ad83822e7
khang-nguyen-anfin-vn <95395268+khang-nguyen-anfin-vn@users.noreply.github.com>
2021-12-22 13:54:11 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
 const express = require('express')
+const request = require('request');
 const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.send('this is the respone from express api-test1 3000')
+  
+  request('http://10.0.8.3', function (error, response, body) {
+    if (error) {
+      res.send('call to http://10.0.8.3 fail. Err ' + error)
+    } else
+    {
+      res.send('call to http://10.0.8.3 success. Code ' + response.statusCode)
+    }
+  });
+
 })
 
 app.listen(port, () => {
